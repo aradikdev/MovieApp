8ecfbd49d7b42bb0d6f5e783485a6ff36aba9a4b
aradikdev <aradikdev@gmail.com>
2023-03-17 19:23:33 +0600

    Add project files.

---
+++ MovieApp/Data/MovieAppContext.cs
@@ -0,0 +1,15 @@
+﻿using MovieApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieApp.Data;
+
+public class MovieAppContext : DbContext
+{
+    public MovieAppContext(DbContextOptions<MovieAppContext> options)
+            : base(options)
+    {
+    }
+
+    public DbSet<Movie> Movie { get; set; }
+    public DbSet<Category> Category { get; set; }
+}
